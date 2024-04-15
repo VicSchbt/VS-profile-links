1fcca6b23739081f0620db0efb79de69302207a8
VicSchbt <victoire.schubert@gmail.com>
2024-04-15 12:49:55 +0200

    fix: icons broken links

--- js/main.js
+++ js/main.js
@@ -1,46 +1,46 @@
-const prefixImageLight = 'assets/';
+const prefixImageLight = "assets/";
 
 const profile = {
-	name: 'Vic Schbt',
-	location: 'Lille, France',
-	labels: ['Software Engineer', 'Software Craftwoman', 'Content Creator'],
-	description:
-		'Working as Web Developer the day, Becoming a Creative Developer at night.',
-	links: [
-		{
-			label: 'Github',
-			url: 'https://github.com/VicSchbt',
-			icon: `${prefixImageLight}icon-github-30x30-black.png`,
-		},
-		{
-			label: 'LinkedIn',
-			url: 'https://www.linkedin.com/feed/',
-			icon: `${prefixImageLight}icon-linkedin-50x50-black.png`,
-		},
-		{
-			label: 'Instagram',
-			url: 'https://www.instagram.com/vicschbt.codes/',
-			icon: `${prefixImageLight}icon-instagram-30x30-black.png`,
-		},
-	],
+  name: "Vic Schbt",
+  location: "Lille, France",
+  labels: ["Software Engineer", "Software Craftwoman", "Content Creator"],
+  description:
+    "Working as Web Developer the day, Becoming a Creative Developer at night.",
+  links: [
+    // {
+    // 	label: 'Github',
+    // 	url: 'https://github.com/VicSchbt',
+    // 	icon: `${prefixImageLight}icon-github-30x30-black.png`,
+    // },
+    {
+      label: "LinkedIn",
+      url: "https://www.linkedin.com/feed/",
+      icon: `${prefixImageLight}icon-linkedin-50x50-black.png`,
+    },
+    {
+      label: "Instagram",
+      url: "https://www.instagram.com/vicschbt.codes/",
+      icon: `${prefixImageLight}icon-instagram-30x30-black.png`,
+    },
+  ],
 };
 
-const nameHeading = document.querySelector('.header-content__name');
-const labelsList = document.querySelector('.header-content__labels');
-const bioParagraph = document.querySelector('.header-content__bio');
-const linksList = document.querySelector('.header-content__links');
+const nameHeading = document.querySelector(".header-content__name");
+const labelsList = document.querySelector(".header-content__labels");
+const bioParagraph = document.querySelector(".header-content__bio");
+const linksList = document.querySelector(".header-content__links");
 
 nameHeading.innerHTML = profile.name;
 bioParagraph.innerHTML = '"' + profile.description + '"';
 
 profile.labels.forEach((label) => {
-	labelsList.innerHTML += `<li class="header-content__label">
+  labelsList.innerHTML += `<li class="header-content__label">
   ${label}
 </li>`;
 });
 
 profile.links.forEach((link) => {
-	linksList.innerHTML += `
+  linksList.innerHTML += `
 	<li class="header-content__link">
 		<a href="${link.url}" target="_blank" aria-label="${link.label}">
 			<img src="${link.icon}" alt="icon ${link.label}" type="image/png"/>
