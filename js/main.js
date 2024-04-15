37bef8085fb736542cd4e8a1fe99770e05433930
VicSchbt <victoire.schubert@gmail.com>
2024-04-15 15:34:19 +0200

    fix: linkedin link

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const profile = {
     },
     {
       label: "LinkedIn",
-      url: "https://www.linkedin.com/feed/",
+      url: "https://www.linkedin.com/victoire-schubert",
       icon: `${iconPrefix}icon-linkedin-50x50-black.png`,
     },
     {
